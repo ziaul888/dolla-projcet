52904c8573d8f34163235ab2bba07c8ec4a26654
ziaul888 <ziaul935@gmail.com>
2021-01-05 05:43:12 +0600

    navbar component

---
+++ src/component/useReducer.jsx
@@ -0,0 +1,36 @@
+import React,{useReducer} from 'react'
+
+const initialState =0;
+const reducer= (state, action)=>{
+    switch(action){
+        case 'increment':
+            return state+1;
+        case 'decrement':
+            return state -1;
+        case 'reset' :
+         return initialState
+         default:
+             return state;       
+    }
+
+}
+
+
+
+
+
+const ConuterOne =()=>{
+
+    const [count, dispatch] = useReducer(reducer, initialState)
+    return(
+        <div>
+            <p>count:{count}</p>
+            <button onClick={()=>dispatch('increment')}>increment</button>
+            <button  onClick={()=>dispatch('decrement')}>decrement</button>
+            <button  onClick={()=>dispatch('reset')}>reset</button>
+        </div>
+    )
+}
+
+
+export default ConuterOne
